b1aadb05af170f3c1c04cd23a290826ed90e8a93
kongyue <kongyue52@126.com>
2020-07-21 20:44:06 +0800

    上传数据

--- molecular_network/mol_dataset/__init__.py
+++ molecular_network/mol_dataset/__init__.py
@@ -1,3 +1,3 @@
 # from .qm9 import QM9
 # from .enamine import Enamine
-from .bace import BACE
+from .dataset import Dataset
--- molecular_network/mol_dataset/dataset.py
+++ molecular_network/mol_dataset/dataset.py
@@ -17,20 +17,21 @@ import rdkit
 from rdkit import Chem
 
 
-class BACE(InMemoryDataset):
+class Dataset(InMemoryDataset):
     '''
     需指定raw_file的文件名,默认为全数据集（1亿一千万分子）
     '''
-    def __init__(self, root, transform=None, pre_transform=None,
+    def __init__(self, root, raw_name, transform=None, pre_transform=None,
                  pre_filter=None):
-        super(BACE, self).__init__(root, transform, pre_transform, pre_filter)
+        self.raw_name = raw_name
+        super(Dataset, self).__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0]) 
 
 
     # 这里只要覆写以下四个方法即可，帮助上一句代码实现load（processed文件）
     @property
     def raw_file_names(self):
-        return 'bace.csv'
+        return self.raw_name
 
     @property
     def processed_file_names(self):
@@ -45,14 +46,21 @@ class BACE(InMemoryDataset):
         
         data_list = []
         i = 0
-        for i,smi in enumerate(smis):
-            
+        for i,smi in enumerate(smis):           
 
             mol = Chem.MolFromSmiles(smi)
 
-            x = atom_feature(mol)
+            if mol.GetNumAtoms() == 1:
+                continue
 
-            edge_index, edge_attr = bond_feature(mol)  
+            try: #遇到特殊分子，无法生成特征的就跳过
+
+                x = atom_feature(mol)
+                edge_index, edge_attr = bond_feature(mol)  
+
+            except:
+                print('第%i个分子被跳过，无法生成节点或者边特征,%s' %(i,smi))
+                continue
 
             y = target[i]
 
@@ -60,8 +68,7 @@ class BACE(InMemoryDataset):
                 print('process %i molecules' %(i))
 
             data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
-            
-                          
+                                     
             data_list.append(data)
 
         torch.save(self.collate(data_list), self.processed_paths[0])
